0e7f908c2a2d7099e69f7ec6b7cd06c3321a23cd
asmachebbi97 <asmachebbi222@gmail.com>
2022-07-08 04:37:12 +0200

    f

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
+from fastapi import FastAPI
 import pickle
 import re
 from adal import AuthenticationContext
 from flask import Flask, request, jsonify, render_template
 from flask_ngrok import run_with_ngrok
+from flask import Flask
+from flask_cors import CORS,cross_origin
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 run_with_ngrok(app)
 
 
 @app.route('/token')
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def AzureAccessToken():
  
   
@@ -30,6 +38,7 @@ def AzureAccessToken():
   auth_context = AuthenticationContext(authority)
 
   token = auth_context.acquire_token_with_username_password(resource, username, password, clientId)
+  
   
    
  
